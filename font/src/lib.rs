70734e5f25b2141469542237e24260a623c264c3
Toby Sullivan <toby.s@canva.com>
2018-07-26 17:27:16 +1000

    Add a bunch of font file types

--- font/src/lib.rs
+++ font/src/lib.rs
@@ -1,6 +1,12 @@
 #[derive(Debug)]
 pub enum FileType {
-    OpenType,
+    OpenTypeWithTrueTypeOutlines, // https://docs.microsoft.com/en-us/typography/opentype/spec/otff
+    OpenTypeWithCFFData, // https://docs.microsoft.com/en-us/typography/opentype/spec/otff
+    OpenTypeFontCollection, // https://docs.microsoft.com/en-us/typography/opentype/spec/otff
+    PostScriptInSfnt, // https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
+    AppleCompatibleTrueType, // https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
+    Woff, // https://www.w3.org/TR/2012/REC-WOFF-20121213/
+    Woff2, // https://www.w3.org/TR/WOFF2/
 }
 
 #[derive(Debug)]
@@ -17,7 +23,13 @@ impl Font {
 
     fn detect_type(content: &Vec<u8>) -> FileType {
         match content[0..4] {
-            [0x00, 0x01, 0x00, 0x00] => FileType::OpenType,
+            [0x00, 0x01, 0x00, 0x00] => FileType::OpenTypeWithTrueTypeOutlines, // 0x00010000
+            [0x4F, 0x54, 0x54, 0x4F] => FileType::OpenTypeWithCFFData, // 'OTTO'
+            [0x74, 0x74, 0x63, 0x66] => FileType::OpenTypeFontCollection, // 'ttcf'
+            [0x74, 0x79, 0x70, 0x31] => FileType::PostScriptInSfnt, // 'typ1'
+            [0x74, 0x72, 0x75, 0x65] => FileType::AppleCompatibleTrueType, // 'true'
+            [0x77, 0x4F, 0x46, 0x46] => FileType::Woff, // 'wOFF'
+            [0x77, 0x4F, 0x46, 0x32] => FileType::Woff2, // 'wOF2'
             _ => panic!("Unrecognised type"),
         }
     }
