4b585516b2eaab10136741a09223e6536a00cf5d
Toby Sullivan <tobyjsullivan@gmail.com>
2018-09-24 20:53:15 +1000

    Parse string lengths and offsets

--- font/src/opentype/name/mod.rs
+++ font/src/opentype/name/mod.rs
@@ -1,4 +1,3 @@
-/// The name table stores strings which represent various metadata in the font (e.g., foundry name, font name, etc.).
 pub mod encoding;
 
 use byteorder::{ByteOrder, BigEndian};
@@ -7,28 +6,29 @@ use self::encoding::{Platform, Encoding};
 
 const U16_LENGTH: usize = 2;
 
-// Name Table header
-const FORMAT_OFFSET: usize = 0;
-const COUNT_OFFSET: usize = 2;
-const STRING_OFFSET_OFFSET: usize = 4;
-const NAME_RECORDS_OFFSET: usize = 6;
-
-// Name Record
-const NAME_RECORD_LENGTH: usize = 12;
-const PLATFORM_ID_OFFSET: usize = 0;
-const ENCODING_ID_OFFSET: usize = 2;
-const LANGUAGE_ID_OFFSET: usize = 4;
-const NAME_ID_OFFSET: usize = 6;
-
+/// The name table stores strings which represent various metadata in
+/// the font (e.g., foundry name, font name, etc.).
 #[derive(Debug)]
 pub struct NameTable {
+  /// NameTable comes in two formats, 0 and 1.
+  /// The latter supports language-tag records.
   pub format: Format,
+  /// The number of name records in the table.
   pub count: u16,
+  /// The offset of the string storage region (from the start of the table).
   pub string_offset: u16,
+  /// The parsed name records.
   pub name_records: Vec<NameRecord>,
 }
 
 impl NameTable {
+  const FORMAT_OFFSET: usize = 0;
+  const COUNT_OFFSET: usize = 2;
+  const STRING_OFFSET_OFFSET: usize = 4;
+  const NAME_RECORDS_OFFSET: usize = 6;
+  const NAME_RECORD_LENGTH: usize = 12;
+
+  /// Deserialize the name table from font file data.
   pub fn deserialize(data: &[u8]) -> Result<Self, ParseError> {
     Ok(NameTable{
       format: Self::parse_format(data)?,
@@ -39,7 +39,7 @@ impl NameTable {
   }
 
   fn parse_format(data: &[u8]) -> Result<Format, ParseError> {
-    match BigEndian::read_u16(&data[FORMAT_OFFSET..FORMAT_OFFSET+U16_LENGTH]) {
+    match BigEndian::read_u16(&data[Self::FORMAT_OFFSET..Self::FORMAT_OFFSET+U16_LENGTH]) {
       0 => Ok(Format::Format0),
       1 => Ok(Format::Format1),
       _ => Err(ParseError::UnknownFormat),
@@ -47,19 +47,19 @@ impl NameTable {
   }
 
   fn parse_record_count(data: &[u8]) -> u16 {
-    BigEndian::read_u16(&data[COUNT_OFFSET..COUNT_OFFSET+U16_LENGTH])
+    BigEndian::read_u16(&data[Self::COUNT_OFFSET..Self::COUNT_OFFSET+U16_LENGTH])
   }
 
   fn parse_string_offset(data: &[u8]) -> u16 {
-    BigEndian::read_u16(&data[STRING_OFFSET_OFFSET..STRING_OFFSET_OFFSET+U16_LENGTH])
+    BigEndian::read_u16(&data[Self::STRING_OFFSET_OFFSET..Self::STRING_OFFSET_OFFSET+U16_LENGTH])
   }
 
   fn parse_name_records(data: &[u8]) -> Result<Vec<NameRecord>, ParseError> {
     let mut records = vec![];
     let num_records = Self::parse_record_count(data);
     for n in 0..num_records {
-      let offset = NAME_RECORDS_OFFSET + n as usize * NAME_RECORD_LENGTH;
-      let record_data = &data[offset..offset+NAME_RECORD_LENGTH];
+      let offset = Self::NAME_RECORDS_OFFSET + n as usize * Self::NAME_RECORD_LENGTH;
+      let record_data = &data[offset..offset+Self::NAME_RECORD_LENGTH];
 
       records.push(NameRecord::deserialize(record_data)?);
     }
@@ -68,13 +68,19 @@ impl NameTable {
   }
 }
 
+/// Errors which result from unexpected data.
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
+  /// Only formats 0 and 1 are recognized.
+  /// Anything else will result in an UnknownFormat error.
   UnknownFormat,
+  /// The Platform ID hasn't been implemented yet.
   UnknownPlatformID,
+  /// The Encoding ID hasn't been implemented yet.
   UnknownEncodingID,
 }
 
+/// Supported formats for Name Tables.
 #[derive(Debug, PartialEq)]
 pub enum Format {
   Format0,
@@ -88,9 +94,18 @@ pub struct NameRecord {
   language_id: u16,
   name_id: u16,
   name: Option<Name>,
+  string_length: u16,
+  string_offset: usize,
 }
 
 impl NameRecord {
+  const PLATFORM_ID_OFFSET: usize = 0;
+  const ENCODING_ID_OFFSET: usize = 2;
+  const LANGUAGE_ID_OFFSET: usize = 4;
+  const NAME_ID_OFFSET: usize = 6;
+  const STRING_LENGTH_OFFSET: usize = 8;
+  const STRING_OFFSET_OFFSET: usize = 10;
+
   fn deserialize(data: &[u8]) -> Result<Self, ParseError> {
     Ok(Self {
       platform: Self::parse_platform(data)?,
@@ -98,32 +113,42 @@ impl NameRecord {
       language_id: Self::parse_language_id(data),
       name_id: Self::parse_name_id(data),
       name: Name::lookup(Self::parse_name_id(data)),
+      string_length: Self::parse_string_length(data),
+      string_offset: Self::parse_string_offset(data),
     })
   }
 
   fn parse_platform(data: &[u8]) -> Result<Platform, ParseError> {
-    let platform_id = BigEndian::read_u16(&data[PLATFORM_ID_OFFSET..PLATFORM_ID_OFFSET+U16_LENGTH]);
+    let platform_id = BigEndian::read_u16(&data[Self::PLATFORM_ID_OFFSET..Self::PLATFORM_ID_OFFSET+U16_LENGTH]);
     encoding::Platform::lookup(platform_id).ok_or(ParseError::UnknownPlatformID)
   }
 
   fn parse_encoding(data: &[u8]) -> Result<Encoding, ParseError> {
     let platform = Self::parse_platform(data)?;
-    let encoding_id = BigEndian::read_u16(&data[ENCODING_ID_OFFSET..ENCODING_ID_OFFSET+U16_LENGTH]);
+    let encoding_id = BigEndian::read_u16(&data[Self::ENCODING_ID_OFFSET..Self::ENCODING_ID_OFFSET+U16_LENGTH]);
     encoding::Encoding::lookup(platform, encoding_id).ok_or(ParseError::UnknownEncodingID)
   }
 
   fn parse_language_id(data: &[u8]) -> u16 {
-    BigEndian::read_u16(&data[LANGUAGE_ID_OFFSET..LANGUAGE_ID_OFFSET+U16_LENGTH])
+    BigEndian::read_u16(&data[Self::LANGUAGE_ID_OFFSET..Self::LANGUAGE_ID_OFFSET+U16_LENGTH])
   }
 
   fn parse_name_id(data: &[u8]) -> u16 {
-    BigEndian::read_u16(&data[NAME_ID_OFFSET..NAME_ID_OFFSET+U16_LENGTH])
+    BigEndian::read_u16(&data[Self::NAME_ID_OFFSET..Self::NAME_ID_OFFSET+U16_LENGTH])
+  }
+
+  fn parse_string_length(data: &[u8]) -> u16 {
+    BigEndian::read_u16(&data[Self::STRING_LENGTH_OFFSET..Self::STRING_LENGTH_OFFSET+U16_LENGTH])
+  }
+
+  fn parse_string_offset(data: &[u8]) -> usize {
+    BigEndian::read_u16(&data[Self::STRING_OFFSET_OFFSET..Self::STRING_OFFSET_OFFSET+U16_LENGTH]) as usize
   }
 }
 
-// Pre-defined metadata fields which apply to all fonts regardless of platform.
-// Not all valid name IDs necessarily correspond to a defined field.
-// Find details for all of these in the MS docs: https://docs.microsoft.com/en-us/typography/opentype/spec/name#name-ids
+/// Pre-defined metadata fields which apply to all fonts regardless of platform.
+/// Not all valid name IDs necessarily correspond to a defined field.
+/// Find details for all of these in the MS docs: https://docs.microsoft.com/en-us/typography/opentype/spec/name#name-ids
 #[derive(Debug, PartialEq)]
 pub enum Name {
   CopyrightNotice,
@@ -227,5 +252,7 @@ mod tests {
     assert_eq!(record.language_id, 0u16);
     assert_eq!(record.name_id, 0);
     assert_eq!(record.name, Some(Name::CopyrightNotice));
+    assert_eq!(record.string_length, 47);
+    assert_eq!(record.string_offset, 0);
   }
 }
