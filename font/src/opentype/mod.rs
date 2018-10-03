005905d6a98cb2f251c480897cba7545a300f7d2
Toby Sullivan <1534986+tobyjsullivan@users.noreply.github.com>
2018-10-03 19:10:05 +1000

    Add table detection for remaining tables (#9)

--- font/src/opentype/mod.rs
+++ font/src/opentype/mod.rs
@@ -46,6 +46,7 @@ impl<'a> OpenTypeFile<'a> {
                 TableType::Cmap => cmap_data = Some(record.table_data(content)),
                 TableType::Name => name_data = Some(record.table_data(content)),
                 TableType::Unknown => {}
+                _ => {}
             }
         }
 
@@ -111,8 +112,107 @@ impl<'a> OpenTypeFile<'a> {
 }
 
 enum TableType {
+    /// Axis variations table
+    Avar,
+    /// Baseline table
+    Base,
+    /// Color bitmap data table
+    Cbdt,
+    /// Color bitmap location table
+    Cblc,
+    /// Compact font format (CFF) table
+    Cff,
+    /// Compact font format (CFF) version 2 table
+    Cff2,
+    /// Character to glyph index mapping table
     Cmap,
+    /// Color table
+    Colr,
+    /// Color palette table
+    Cpal,
+    /// CVT variations table
+    Cvar,
+    /// Control value table
+    Cvt,
+    /// Digital signature table
+    Dsig,
+    /// Embedded bitmap data table
+    Ebdt,
+    /// Embedded bitmap location table
+    Eblc,
+    /// Embedded bitmap scaling table
+    Ebsc,
+    /// Font program table
+    Fpgm,
+    /// Font variations table
+    Fvar,
+    /// Grid-fitted and scan-conversion procedure table
+    Gasp,
+    /// Glyph definition table
+    Gdef,
+    /// Glyph data table
+    Glyf,
+    /// Glyph position table
+    Gpos,
+    /// Glyph substitution table
+    Gsub,
+    /// Glyph variations table
+    Gvar,
+    /// Horizontal device metrics table
+    Hdmx,
+    /// Font header table
+    Head,
+    /// Horizontal header table
+    Hhea,
+    /// Horizontal metrics table
+    Hmtx,
+    /// Horizontal metrics variations table
+    Hvar,
+    /// Justification table
+    Jstf,
+    /// Kerning table
+    Kern,
+    /// Index to location table
+    Loca,
+    /// Linear threshold table
+    Ltsh,
+    /// Mathmatical typesetting table
+    Math,
+    /// Maximum profile table
+    Maxp,
+    /// Merge table
+    Merg,
+    /// Metadata table
+    Meta,
+    /// Metrics variations table
+    Mvar,
+    /// Naming table
     Name,
+    /// OS/2 and Windows metrics table
+    Os2,
+    /// PCL 5 table
+    Pclt,
+    /// PostScript table
+    Post,
+    /// Control value program table
+    Prep,
+    /// Standard bitmap graphics table
+    Sbix,
+    /// Style attributes table
+    Stat,
+    /// SVG table
+    Svg,
+    /// Vertical device metrics table
+    Vdmx,
+    /// Vertical header table
+    Vhea,
+    /// Vertical metrics table
+    Vmtx,
+    /// Vertical origin table
+    Vorg,
+    /// Vertical metrics variations table
+    Vvar,
+    /// A placeholder for any unrecognised table.
     Unknown,
 }
 
@@ -136,8 +236,57 @@ impl TableRecord {
 
     fn table_type(&self) -> TableType {
         match self.tag {
+            // Tags with fewer than four characters, such as cvt, pad spaces on the end.
+            ['a', 'v', 'a', 'r'] => TableType::Avar,
+            ['B', 'A', 'S', 'E'] => TableType::Base,
+            ['C', 'B', 'D', 'T'] => TableType::Cbdt,
+            ['C', 'B', 'L', 'C'] => TableType::Cblc,
+            ['C', 'F', 'F', ' '] => TableType::Cff,
+            ['C', 'F', 'F', '2'] => TableType::Cff2,
             ['c', 'm', 'a', 'p'] => TableType::Cmap,
+            ['C', 'O', 'L', 'R'] => TableType::Colr,
+            ['C', 'P', 'A', 'L'] => TableType::Cpal,
+            ['c', 'v', 'a', 'r'] => TableType::Cvar,
+            ['c', 'v', 't', ' '] => TableType::Cvt,
+            ['D', 'S', 'I', 'G'] => TableType::Dsig,
+            ['E', 'B', 'D', 'T'] => TableType::Ebdt,
+            ['E', 'B', 'L', 'C'] => TableType::Eblc,
+            ['E', 'B', 'S', 'C'] => TableType::Ebsc,
+            ['f', 'p', 'g', 'm'] => TableType::Fpgm,
+            ['f', 'v', 'a', 'r'] => TableType::Fvar,
+            ['g', 'a', 's', 'p'] => TableType::Gasp,
+            ['G', 'D', 'E', 'F'] => TableType::Gdef,
+            ['g', 'l', 'y', 'f'] => TableType::Glyf,
+            ['G', 'P', 'O', 'S'] => TableType::Gpos,
+            ['G', 'S', 'U', 'B'] => TableType::Gsub,
+            ['g', 'v', 'a', 'r'] => TableType::Gvar,
+            ['h', 'd', 'm', 'x'] => TableType::Hdmx,
+            ['h', 'e', 'a', 'd'] => TableType::Head,
+            ['h', 'h', 'e', 'a'] => TableType::Hhea,
+            ['h', 'm', 't', 'x'] => TableType::Hmtx,
+            ['H', 'V', 'A', 'R'] => TableType::Hvar,
+            ['J', 'S', 'T', 'F'] => TableType::Jstf,
+            ['k', 'e', 'r', 'n'] => TableType::Kern,
+            ['l', 'o', 'c', 'a'] => TableType::Loca,
+            ['L', 'T', 'S', 'H'] => TableType::Ltsh,
+            ['M', 'A', 'T', 'H'] => TableType::Math,
+            ['m', 'a', 'x', 'p'] => TableType::Maxp,
+            ['M', 'E', 'R', 'G'] => TableType::Merg,
+            ['m', 'e', 't', 'a'] => TableType::Meta,
+            ['M', 'V', 'A', 'R'] => TableType::Mvar,
             ['n', 'a', 'm', 'e'] => TableType::Name,
+            ['O', 'S', '/', '2'] => TableType::Os2,
+            ['p', 'c', 'l', 't'] => TableType::Pclt,
+            ['p', 'o', 's', 't'] => TableType::Post,
+            ['p', 'r', 'e', 'p'] => TableType::Prep,
+            ['s', 'b', 'i', 'x'] => TableType::Sbix,
+            ['S', 'T', 'A', 'T'] => TableType::Stat,
+            ['S', 'V', 'G', ' '] => TableType::Svg,
+            ['V', 'D', 'M', 'X'] => TableType::Vdmx,
+            ['v', 'h', 'e', 'a'] => TableType::Vhea,
+            ['v', 'm', 't', 'x'] => TableType::Vmtx,
+            ['V', 'O', 'R', 'G'] => TableType::Vorg,
+            ['V', 'V', 'A', 'R'] => TableType::Vvar,
             _ => TableType::Unknown,
         }
     }
