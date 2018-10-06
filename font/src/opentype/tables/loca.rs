a2e44a45ce574992c2df3be9111ed86410d72fee
Toby Sullivan <tobyjsullivan@gmail.com>
2018-10-07 10:42:38 +1100

    WIP

--- font/src/opentype/tables/loca.rs
+++ font/src/opentype/tables/loca.rs
@@ -1,10 +1,12 @@
+use super::maxp::Version;
+
 #[derive(Debug)]
 pub struct LocaTable {
 
 }
 
 impl LocaTable {
-  pub fn parse(table_data: &[u8]) -> Self {
+  pub fn parse(table_data: &[u8], version: Version, num_glyphs: u16) -> Self {
     // TODO: Can't parse until I know if it's short or long format.
     Self {}
   }
